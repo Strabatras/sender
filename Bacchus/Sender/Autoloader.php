01da9889057b7ca54c3d4237f65d774523030568
Strabatras <strabatras@gmail.com>
2020-01-03 20:07:57 +0300

    Добавлена реализация автозагрузчика

---
+++ Bacchus/Sender/Autoloader.php
@@ -0,0 +1,64 @@
+<?php
+namespace Bacchus\Sender;
+
+/**
+ * Class Autoloader
+ * @package Bacchus\Sender
+ */
+class Autoloader {
+
+    private $nameSpaceMapping = [];
+
+    /**
+     * Регистрирует заданную функцию в качестве реализации метода
+     */
+    public function splAutoloadRegister() {
+        spl_autoload_register( [ $this, 'loadClass' ] );
+    }
+
+    /**
+     * Функция, реализующая метод spl_autoload()
+     * @param string $class
+     */
+    protected function loadClass( string $class ){
+        $nameSpaceMapping = $this->nameSpaceMapping();
+        if ( key_exists( $class, $nameSpaceMapping ) ) {
+            require_once( $nameSpaceMapping[ $class ] );
+        }
+    }
+
+    /**
+     * Заполняет массив соответствий неймспейсов и путей файлов классов
+     * @param string $nameSpace неймспейс
+     * @param string $path стартовая директория
+     */
+    protected function fillNameSpaceMapping( string $nameSpace, string $path ){
+        $scanned = array_diff( scandir( $path ), [ '..', '.' ] );
+        foreach ( $scanned AS $row ) {
+            $fileInfo = new \SplFileInfo( $path . DIRECTORY_SEPARATOR . $row );
+            if ( $fileInfo->isDir() ) {
+                $this->fillNameSpaceMapping( $nameSpace . '\\' . $row, $path . DIRECTORY_SEPARATOR . $row );
+            } elseif ( strtolower( $fileInfo->getExtension() ) === 'php' ) {
+                $this->nameSpaceMapping[ $nameSpace . '\\' . $fileInfo->getBasename('.' . $fileInfo->getExtension() ) ] = $fileInfo->getPathname();
+            }
+        }
+    }
+
+    /**
+     * Массив соответствий неймспейсов и путей файлов классов
+     * @return array
+     */
+    protected function nameSpaceMapping() : array {
+        return $this->nameSpaceMapping;
+    }
+
+    /**
+     * Соответствие неймспейса и стартовой директории
+     * @param string $nameSpace неймспейс
+     * @param string $path стартовая директория
+     */
+    public function nameSpaceRegister( string $nameSpace, string $path ){
+        $this->fillNameSpaceMapping( $nameSpace, $path );
+    }
+
+}
--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+
+include_once( __DIR__ . DIRECTORY_SEPARATOR . 'Bacchus' . DIRECTORY_SEPARATOR .  'Sender' . DIRECTORY_SEPARATOR . 'Autoloader.php' );
+
+$autoloader = new Bacchus\Sender\Autoloader();
+$autoloader->SplAutoloadRegister();
+$autoloader->nameSpaceRegister( 'Bacchus\Sender', __DIR__ . DIRECTORY_SEPARATOR . 'Bacchus' . DIRECTORY_SEPARATOR . 'Sender' . DIRECTORY_SEPARATOR . 'src' );
+
+$sender = new \Bacchus\Sender\Sender();
+
+
+echo "<pre>";
+print_r( $sender );
+echo "</pre>";
